05d6c85111df9cfdfc082d42e5ef8a7793ce841a
José Ibañez <jibanez123456@gmail.com>
2021-01-18 18:16:41 -0300

    Updated stage nexusUpload

--- vars/mavenCI.groovy
+++ vars/mavenCI.groovy
@@ -92,17 +92,20 @@ def call(){
 
             try {
                 // Nombre extraido desde Jenkins > Global tool configuration > SonarQube Scanner
-                def scannerHome = tool 'sonar-scanner';
+                def scannerHome = tool 'sonar_scanner';
 
                 // Nombre extraido desde Jenkins > Configurar el sistema > SonarQube servers
                 withSonarQubeEnv('sonar_server') {
 
                     if (isUnix()) {
-                        sh "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+                        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ms-iclab -Dsonar.java.binaries=build"    
+
                     } else {
-                        bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+                        bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ms-iclab -Dsonar.java.binaries=build"
                     }
                 }
+
+            }
 
                 // Si pasa el try el step fue exitoso
                 success = true
@@ -120,26 +123,27 @@ def call(){
             env.STAGE = STAGE_NAME
 
             try {
-                nexusPublisher nexusInstanceId: 'nexus',
-                    nexusRepositoryId: 'laboratorio-grupo-4',
+                nexusPublisher nexusInstanceId: 'nexus', 
+                    nexusRepositoryId: 'laboratorio-grupo-4', 
                     packages: [
-                        [
-                            $class: 'MavenPackage',
-                            mavenAssetList: [
-                                [
-                                    classifier: '',
-                                    extension: 'jar',
-                                    filePath: 'C:\\proyects\\diplomado\\gradle\\ejemplo-gradle\\build\\DevOpsUsach2020-0.0.1.jar'
-                                ]
-                            ],
-                            mavenCoordinate: [
-                                artifactId: 'DevOpsUsach2020',
-                                groupId: 'com.devopsusach2020',
-                                packaging: 'jar',
-                                version: '0.0.1'
+                    [
+                        $class: 'MavenPackage', 
+                        mavenAssetList: [
+                            [
+                                classifier: '', 
+                                extension: 'jar', 
+                                filePath: './build/DevOpsUsach2020-0.0.1.jar'
                             ]
+                        ], 
+                        mavenCoordinate: [
+                            artifactId: 'DevOpsUsach2020', 
+                            groupId: 'com.devopsusach2020', 
+                            packaging: 'jar', 
+                            version: '0.0.1'
                         ]
                     ]
+                ] 
+
                 
                 // Si pasa el try el step fue exitoso
                 success = true
