e3b3e375d882199c8643bea40a9f0fcf99f95fc7
Guillermo Correa M <guillermocorreamartinez@gmail.com>
2021-01-19 17:55:00 -0300

    Se modifica mavenCD

--- vars/mavenCD.groovy
+++ vars/mavenCD.groovy
@@ -4,89 +4,74 @@ import pipeline.utils.GitMethods
 //def call(stage_param, branch_name){
 // def call(String choosenStages, String pipelineStages){
 def call(){
-        def validador = new Validator()
+    def validador = new Validator()
         
-        def stages = ['gitDiff','nexusDownload','runJar','test','gitMergeMaster','gitDevelop','gitTagMaster']
+    def stages = ['gitDiff','nexusDownload','runJar','test','gitMergeMaster','gitMergeDevelop','gitTagMaster']
 
-        def so = isUnix() ? 'Linux' : 'Windows'
-        figlet so
+    def so = isUnix() ? 'Linux' : 'Windows'
+    figlet so
  
-        stages.each{
-                if (validador.isValidStage(it, params.stage)){       
-                        stage(it){
-                                try{
-                                        "$it"()
-                                } catch( Exception e){
-                                        error "Stage ${it} tiene problemas : ${e}"
-                                }
-
-                        }
+    stages.each{
+        if (validador.isValidStage(it, params.stage)){       
+            stage(it){
+                try {
+                    "$it"()
+                } catch (Exception e) {
+                    error "Stage ${it} tiene problemas : ${e}"
                 }
+
+            }
         }
- }
+    }
+}
 
 def gitDiff(){
-                script {
-                        env.ETAPA = 'GitDiff'
-                        figlet env.ETAPA
-                }
-         env.RELV = sh ( script: " echo ${GIT_BRANCH}|sed -n 's/.*\\(release-v.-.-.*\\).*/\\1/p' ", returnStdout:true )
-//        if (env.GIT_BRANCH.contains('*release*')){
-        if ( env.RELV.contains('release-v1-0-0')){
-
-            def git = new GitMethods()
-
-            if (git.checkIfBranchExists('master')){
-                echo "Rama existe"
-                git.diffMerge('master','release-v1-0-0')
-            } else {
-                                echo "no existe master, verificar branch"
-            }} else {
-            echo "la rama ${env.GIT_BRANCH} no corresponde como rama release, no se puede hacer delivery"
-        }
-        }
+    script {
+        env.ETAPA = 'GitDiff'
+        figlet env.ETAPA
+    }
+    sh 'git diff origin/main...' + env.GIT_BRANCH
+}
 
 
 def nexusDownload() {
-            script {
-                        env.ETAPA = 'NexusDownload'
-                        figlet env.ETAPA
-                }
-                if (isUnix()) {
-                    sh "curl -X GET -u admin:devops4 http://34.229.88.5:8085/repository/laboratorio-grupo-4/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
-                } else {
-                    bat "curl -X GET -u admin:devops4 http://34.229.88.5:8085/repository/laboratorio-grupo-4/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
-                }
-                
+    script {
+        env.ETAPA = 'NexusDownload'
+        figlet env.ETAPA
     }
+    if (isUnix()) {
+        sh "curl -X GET -u admin:devops4 http://34.229.88.5:8085/repository/laboratorio-grupo-4/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
+    } else {
+        bat "curl -X GET -u admin:devops4 http://34.229.88.5:8085/repository/laboratorio-grupo-4/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
+    }
+}
 
 def runJar() {
-                script {
-                        env.ETAPA = 'Run'
-                        figlet env.ETAPA
-                }
-                if (isUnix()) {
-                        sh 'nohup java -jar DevOpsUsach2020-0.0.1.jar &'
-                } else {
-                        bat 'start /B mvn spring-boot:run'
-                        sleep 60
-                }
+    script {
+        env.ETAPA = 'Run'
+        figlet env.ETAPA
+    }
+    if (isUnix()) {
+        sh 'nohup java -jar DevOpsUsach2020-0.0.1.jar &'
+    } else {
+        bat 'start /B mvn spring-boot:run'
+        sleep 60
+    }
 }
 
 def test() {
-                script {
-                        env.ETAPA = 'Test'
-                        figlet env.ETAPA
-                }
-                if (isUnix()) {
-                        sh 'sleep 20'
-                        sh "curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
-                } else {
-                        bat 'sleep 20'
-                        bat "curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
-                }
-
-        }
+    script {
+        env.ETAPA = 'Test'
+        figlet env.ETAPA
+    }
+    if (isUnix()) {
+        sh 'sleep 20'
+        sh "curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
+    } else {
+        bat 'sleep 20'
+        bat "curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
+    }
+}
 
 def gitMergeMaster() {
     script {
