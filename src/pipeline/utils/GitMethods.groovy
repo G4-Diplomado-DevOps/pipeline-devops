0ed917aad5422529fcb25b84a4027511f563c70d
Adrian <adriangomez.cl@gmail.com>
2021-01-18 23:43:11 -0300

    Agregando git pull

--- src/pipeline/utils/GitMethods.groovy
+++ src/pipeline/utils/GitMethods.groovy
@@ -15,8 +15,8 @@ def checkIfBranchExists(String branch) {
 }
 
 def deleteBranch(String branch) {
-	sh 'git push origin --delete ' + branch
-	sh 'git branch -d ' + branch
+	sh 'git pull; git push origin --delete ' + branch
+	sh 'git branch -D ' + branch
 }
 
 
