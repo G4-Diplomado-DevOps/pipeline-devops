77b13a249dd6ca43e79ece554230a02b5556f65b
aelguetac <73565690+aelguetac@users.noreply.github.com>
2021-01-19 00:19:53 -0300

    Update mavenCD.groovy

--- vars/mavenCD.groovy
+++ vars/mavenCD.groovy
@@ -60,7 +60,7 @@ def nexusDownload() {
                 sh "curl -X GET -u admin:devops4 http://34.229.88.5:8085/repository/laboratorio-grupo-4/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
     }
 
-def runJar() {
+def run() {
                 script {
                         env.ETAPA = 'Run'
                         figlet env.ETAPA
